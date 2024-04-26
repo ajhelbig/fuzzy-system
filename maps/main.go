0d05a74b64dd8782796383e47d522ef529c98a0c
Andrew Helbig <helbigventures@gmail.com>
2024-04-26 09:34:23 -0700

    finished up maps section

--- maps/main.go
+++ maps/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("maps")
+	colors := map[string]string{
+		"red": "#FF0000",
+		"green": "#00FF00",
+		"blue": "#0000FF",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
